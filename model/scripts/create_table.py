23325da8f4da70754e38421dbbf23e99090c2660
jnnr <32454596+jnnr@users.noreply.github.com>
2021-01-14 11:43:16 +0100

    Clean up

--- model/scripts/create_table.py
+++ model/scripts/create_table.py
@@ -5,14 +5,62 @@ import pandas as pd
 from helper import get_experiment_dirs, get_scenario_assumptions
 
 
+dirs =get_experiment_dirs('all_scenarios')
+
+input_file_path = os.path.join(dirs['postprocessed'], 'scalars.csv')
+
+all_scalars = pd.read_csv(input_file_path)
+
+
+def filter_and_round(df, filter, scenario_select, decimals):
+    _df = df.copy()
+
+    _df = _df.loc[all_scalars['var_name'] == filter]
+
+    _df = _df[['scenario', 'var_value']].set_index('scenario')
+
+    _df = _df.loc[scenario_select, :]
+
+    _df = _df.round(decimals)
+
+    return _df
+
+
+def filter_assumptions(scenario_assumptions, scenario_select, assumption_select):
+    assumptions = scenario_assumptions.loc[
+        scenario_assumptions['scenario'].isin(scenario_select)]
+
+    assumptions = assumptions.set_index('scenario')
+
+    assumptions = assumptions[assumption_select]
+
+    return assumptions
+
+
+def relative_values(assumptions):
+    assumptions['overnight_cost_heat_pump'] *= \
+        100 / assumptions.loc['SQ', 'overnight_cost_heat_pump']
+
+    assumptions['overnight_cost_heat_pump'] = \
+        assumptions['overnight_cost_heat_pump'].round(0).astype('int')
+
+    assumptions['overnight_cost_heat_pump'] *= \
+        100 / assumptions.loc['SQ', 'overnight_cost_heat_pump']
+
+    assumptions['overnight_cost_heat_pump'] = \
+        assumptions['overnight_cost_heat_pump'].round(0).astype('int')
+
+    return assumptions
+
+
+def save_df(df, name):
+    output_file_path = os.path.join(dirs['tables'], name)
+
+    df.to_csv(output_file_path)
+
+
 def main(scenario_assumptions):
     print("Creating table of scenario assumptions and results")
-
-    dirs = get_experiment_dirs('all_scenarios')
-
-    input_file_path = os.path.join(dirs['postprocessed'], 'scalars.csv')
-
-    all_scalars = pd.read_csv(input_file_path)
 
     scenario_select = [
         'SQ',
@@ -31,31 +79,6 @@ def main(scenario_assumptions):
         'FF-Mean-150-Std-150',
     ]
 
-    def filter_and_round(df, filter, decimals):
-
-        _df = df.copy()
-
-        _df = _df.loc[all_scalars['var_name'] == filter]
-
-        _df = _df[['scenario', 'var_value']].set_index('scenario')
-
-        _df = _df.loc[scenario_select, :]
-
-        _df = _df.round(decimals)
-
-        return _df
-
-    def save_df(df, name):
-
-        output_file_path = os.path.join(dirs['tables'], name)
-
-        df.to_csv(output_file_path)
-
-
-    assumptions = scenario_assumptions.loc[scenario_assumptions['scenario'].isin(scenario_select)]
-
-    assumptions = assumptions.set_index('scenario')
-
     assumption_select = [
         'charges_tax_levies_gas',
         'charges_tax_levies_el',
@@ -65,17 +88,20 @@ def main(scenario_assumptions):
         'cop_heat_pump',
     ]
 
-    assumptions = assumptions[assumption_select]
+    assumptions = filter_assumptions(scenario_assumptions, scenario_select, assumption_select)
 
-    share_el_heat = filter_and_round(all_scalars, 'share_el_heat', 2)
+    assumptions = relative_values(assumptions)
+
+    share_el_heat = \
+        filter_and_round(all_scalars, 'share_el_heat', scenario_select, decimals=2).astype('int')
 
     share_el_heat *= 100  # decimals to percent
 
-    spec_cost_of_heat = filter_and_round(all_scalars, 'spec_cost_of_heat', 0)
+    spec_cost_of_heat = \
+        filter_and_round(all_scalars, 'spec_cost_of_heat', scenario_select, decimals=0) \
+            .astype('int')
 
     results = pd.concat([assumptions, share_el_heat, spec_cost_of_heat], 1, sort=True)
-
-    results = results.astype('int')
 
     save_df(results, name='results.csv')
 
